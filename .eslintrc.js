cf9aec2b767e229d8e914d220310262c35c372ba
Miguel Montalvo <miguel.montalvo@barrelny.com>
2020-04-19 13:25:32 -0700

    Add .eslintrc.js file
    So that the ESlint loader for webpack doesn't complain of
    lib/hot-client.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "env": {
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "rules": {
+    "semi": ["error", "never"],
+    "quotes": ["error", "single", { "avoidEscape": true, "allowTemplateLiterals": false }]
+  }
+};
